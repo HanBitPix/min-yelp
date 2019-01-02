64a913e1a50fe8b0b46721865de51c1fb1ca9e65
HanBitPix <hanjt517@gmail.com>
2019-01-02 15:50:08 -0800

    Basic Passport Google Strategy Finished

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  yelpClientID: process.env.YELP_CLIENT_ID,
+  yelpApiKey: process.env.YELP_API_KEY,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
---
+++ models/User.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleID: String
+});
+
+mongoose.model('users', userSchema);
